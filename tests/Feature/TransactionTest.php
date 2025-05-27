43e2e17da5a3e55623c3d19e4036eb830f92109a
Shift <shift@laravelshift.com>
2025-05-27 01:26:56 +0000

    Add return types to test methods

--- tests/Feature/TransactionTest.php
+++ tests/Feature/TransactionTest.php
@@ -11,7 +11,7 @@ class TransactionTest extends TestCase
 {
     const TD_CANADA = 'ins_42';
 
-    public function testGetTransactions()
+    public function testGetTransactions(): void
     {
         try {
             Plaid::environment('sandbox');
